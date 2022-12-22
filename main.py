856d0fd1af368326636457b09d11f0785ac68ca5
confusedDip <anihero4@gmail.com>
2022-12-22 13:16:16 -0800

    Added the routes, basic server setup

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=['GET'])
+def home():
+    """Home Page: The Landing Page of the app"""
+    return "<p>This is the Home Page</p>"
+
+
+@app.route("/signup", methods=['GET'])
+def signup():
+    """Send the user to Globus Auth with signup=1."""
+    return "<p>This is the Sign Up Page</p>"
+
+
+@app.route("/login", methods=['GET'])
+def login():
+    """Send the user to Globus Auth."""
+    return "<p>Log In Page"
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=8080, debug=True)
